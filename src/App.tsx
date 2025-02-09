9a17989d1a10c8fa30c8c548bd60bd1ca80ed8b6
Rodrigo Santos <rodrigo.santos.ii@hotmail.com>
2025-02-09 11:10:19 -0300

    :sparkles: feat: restructure App component to include Home, About, Services, Gallery, and Contact sections

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
+import { Contact } from "lucide-react";
+import About from "./components/about/About";
+import Gallery from "./components/gallery/Gallery";
+import Home from "./components/home/Home";
+import ServicesSection from "./components/works/ServicesSection";
+import Footer from "./components/footer/Footer";
+
 const App = () => {
   return (
-    <div className="min-h-screen bg-rose-50">
-    </div>
+    <main className="min-h-screen bg-rose-50">
+      <Home />
+      <About />
+      <ServicesSection />
+      <Gallery />
+      <Contact />
+      <Footer />
+    </main>
   );
 }
 
