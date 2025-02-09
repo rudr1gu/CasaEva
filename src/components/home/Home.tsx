a5973bcf4a6b69dd9a5f8b05376ef18f533c702b
Rodrigo Santos <rodrigo.santos.ii@hotmail.com>
2025-02-09 16:21:21 -0300

    :sparkles: feat: add logo to Home component and update background image

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,19 +1,20 @@
 import { Calendar } from "lucide-react"
+import logo from "../../assets/logo.png"
+
 
 const Home = () => {
     return (
         <header className="relative h-screen flex items-center justify-center">
             <div className="absolute inset-0">
                 <img
-                    src="https://images.unsplash.com/photo-1560066984-138dadb4c035?auto=format&fit=crop&q=80"
+                    src="https://img.freepik.com/vetores-gratis/fundo-de-desenho-floral-linear_23-2149130875.jpg?t=st=1739128803~exp=1739132403~hmac=d31b808e8b1589ba95cbd9ee94855e18271c8549328809d4882d5c31c0cf1d15&w=1380"
                     alt="Salão de Beleza"
                     className="w-full h-full object-cover"
                 />
                 <div className="absolute inset-0 bg-gradient-to-b from-rose-100/80 to-rose-50/90"></div>
             </div>
-            <div className="relative z-10 text-center px-4">
-                <h1 className="text-6xl md:text-8xl font-serif text-rose-900 mb-4">Casa Eva</h1>
-                <p className="text-xl md:text-2xl text-rose-700 mb-8 font-light">Beleza Natural & Bem-estar</p>
+            <div className="relative z-10 text-center px-4 flex flex-col items-center gap-4">
+                <img src={logo} alt="logo da casa eva" className="h-1/3 w-1/3" />
                 <a
                     href="#agendamento"
                     className="inline-flex items-center gap-2 bg-rose-700 text-white px-8 py-3 rounded-full font-medium hover:bg-rose-800 transition-colors"
